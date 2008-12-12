a1ef0f5926cb04aa083dec40a08e41bf4e411374
Alexey.Zlobin <Alexey.Zlobin@26f4872a-c12e-11dd-927d-974589228060>
2008-12-12 22:06:49 +0000

    РАссчёт траектории движения робота

--- server/src/org/osll/roboracing/server/game/Utils.java
+++ server/src/org/osll/roboracing/server/game/Utils.java
@@ -3,6 +3,7 @@ package org.osll.roboracing.server.game;
 import static java.lang.Math.sqrt;
 
 import org.osll.roboracing.world.Coordinate;
+import org.osll.roboracing.world.Robot;
 import org.osll.roboracing.world.Speed;
 
 public final class Utils {
@@ -17,5 +18,14 @@ public final class Utils {
 	
 	public static double module(double x, double y) {
 		return sqrt(y*y + x*x);
+	}
+
+	public static double module(Coordinate normal) {
+		return module(normal.getX(), normal.getY());
+	}
+
+	public static Coordinate plus(Robot robot, Coordinate normal) {
+		// TODO Auto-generated method stub
+		return null;
 	}
 }
