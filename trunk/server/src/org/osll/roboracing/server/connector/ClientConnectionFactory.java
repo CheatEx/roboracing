752bf795a08a9073fb574bdbe821d1cf3f2a6b07
Pavel.Zubarev <Pavel.Zubarev@26f4872a-c12e-11dd-927d-974589228060>
2008-12-12 13:18:19 +0000

    add Connection factory for gamer

---
+++ trunk/server/src/org/osll/roboracing/server/connector/ClientConnectionFactory.java
@@ -0,0 +1,34 @@
+package org.osll.roboracing.server.connector;
+
+import org.osll.roboracing.world.ServerConnection;
+
+public class ClientConnectionFactory {
+
+	public enum Type {
+		RMI,
+		CORBA,
+		TCP,
+		UDP
+	}
+	
+	public static ServerConnection getConnection(Type type) {
+		ServerConnection con = null;
+		switch (type) {
+		case RMI:
+			con = new org.osll.roboracing.server.connector.rmi.client.ServerConnectionImpl();
+		    break;
+	    case CORBA:
+	    	con = new org.osll.roboracing.server.connector.rmi.client.ServerConnectionImpl();
+	    	break;
+	    case TCP:
+	    	con = new org.osll.roboracing.server.connector.tcp.ServerConnectionImpl(DefaultOptions.getHost(),DefaultOptions.getTcpPort());
+	    	break;
+	    case UDP:
+	    	con = new org.osll.roboracing.server.connector.udp.ServerConnectionImpl(DefaultOptions.getHost(),DefaultOptions.getTcpPort());
+	    	break;
+		default:
+			break;
+		}
+		return con;
+	}
+}
