1443a58eae50144751b09f7e8f779dab2d8b7375
oakjumper <oakjumper@26f4872a-c12e-11dd-927d-974589228060>
2008-12-12 02:04:22 +0000

    constructors using fields

--- trunk/shared/src/org/osll/roboracing/world/Hill.java
+++ trunk/shared/src/org/osll/roboracing/world/Hill.java
@@ -5,4 +5,8 @@ package org.osll.roboracing.world;
  */
 public class Hill extends WorldObject {
 
+	public Hill(double x, double y, double r) {
+		super(x, y, r);
+	}
+
 }
--- trunk/shared/src/org/osll/roboracing/world/Pit.java
+++ trunk/shared/src/org/osll/roboracing/world/Pit.java
@@ -5,4 +5,8 @@ package org.osll.roboracing.world;
  */
 public class Pit extends WorldObject {
 
+	public Pit(double x, double y, double r) {
+		super(x, y, r);
+	}
+
 }
